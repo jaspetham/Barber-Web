4baaaf79785d73c305d60ead7505b7fcbfce269b
jaspertham <jaspertham98@gmail.com>
2020-08-19 13:56:46 +0800

    navbar completed

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
